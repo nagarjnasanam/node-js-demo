7c547392e90ae2365aa8ba85c13e1a52353318e0
Phani Murari <imphanimurari1@gmail.com>
2023-03-23 18:18:08 +0530

    deploying node application

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const path = require("path");
+const bp = require("body-parser");
+const cors = require("cors");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const port = process.env.PORT || 3001;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(port, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// Get Books API
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+  SELECT
+    *
+  FROM
+    book
+  ORDER BY
+    book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+// User Register API
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+
+  console.log(request.body, "request");
+
+  const selectUserQuery = `
+    SELECT 
+      * 
+    FROM 
+      user 
+    WHERE 
+      username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const hashedPassword = password;
+    console.log(hashedPassword, "hashedPassword");
+    if (hashedPassword !== undefined) {
+      console.log(hashedPassword, "inside hashed password");
+      const createUserQuery = `
+     INSERT INTO
+      user (username, name, password, gender, location)
+     VALUES
+      (
+       '${username}',
+       '${name}',
+       '${hashedPassword}',
+       '${gender}',
+       '${location}'  
+      );`;
+      await db.run(createUserQuery);
+      response.status(200).json({ success: true });
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+// User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `
+    SELECT
+      *
+    FROM
+      user
+    WHERE 
+      username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      response.send("Login Success!");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
