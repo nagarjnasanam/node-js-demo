7792f86c8146e47c94310c36948aad9187bc81fc
Phani Murari <imphanimurari1@gmail.com>
2023-03-23 23:32:39 +0530

    fixing issues in login

--- index.js
+++ index.js
@@ -47,70 +47,38 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
-// Get Books API
-app.get("/books/", async (request, response) => {
-  const getBooksQuery = `
-  SELECT
-    *
-  FROM
-    book
-  ORDER BY
-    book_id;`;
-  const booksArray = await db.all(getBooksQuery);
-  response.send(booksArray);
-});
-
 // User Register API
 app.post("/users/", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
-
-  console.log(request.body, "request");
-
-  const selectUserQuery = `
-    SELECT 
-      * 
-    FROM 
-      user 
-    WHERE 
-      username = '${username}';`;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
-    const hashedPassword = password;
-    console.log(hashedPassword, "hashedPassword");
-    if (hashedPassword !== undefined) {
-      console.log(hashedPassword, "inside hashed password");
-      const createUserQuery = `
-     INSERT INTO
-      user (username, name, password, gender, location)
-     VALUES
-      (
-       '${username}',
-       '${name}',
-       '${hashedPassword}',
-       '${gender}',
-       '${location}'  
-      );`;
-      await db.run(createUserQuery);
-      response.status(200).json({ success: "User Successfully Registered" });
-    }
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created new user with ${newUserId}`);
   } else {
-    response.status(400);
+    response.status = 400;
     response.send("User already exists");
   }
 });
 
-// User Login API
-app.post("/login/", async (request, response) => {
-  const { username, password } = request.body;
-  const selectUserQuery = `
-    SELECT
-      *
-    FROM
-      user
-    WHERE 
-      username = '${username}';`;
-  const dbUser = await db.get(selectUserQuery);
 
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
     response.status(400);
     response.send("Invalid User");
@@ -124,3 +92,5 @@ app.post("/login/", async (request, response) => {
     }
   }
 });
+
+module.exports = app;
